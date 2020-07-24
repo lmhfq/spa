df174cbf189c992e46c9749afbdc728b89f7e6d3
lmh <lmh@weiyian.com>
2020-07-24 17:14:14 +0800

    add

---
+++ api/src/main/java/com/lmh/spa/api/dto/UserAddDTO.java
@@ -0,0 +1,17 @@
+package com.lmh.spa.api.dto;
+
+import java.io.Serializable;
+
+public class UserAddDTO implements Serializable {
+
+    /**
+     * 昵称
+     */
+    private String name;
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+
+}
